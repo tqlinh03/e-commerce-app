2fca091ba15ee5fbabf638ccab4c2b9c9cb18178
tqlinh03 <truongquanglinh2310@gmail.com>
2025-04-01 09:55:37 +0700

    update entity product

--- service/product/src/main/java/com/linh/ecommerce/product/Product.java
+++ service/product/src/main/java/com/linh/ecommerce/product/Product.java
@@ -1,9 +1,9 @@
 package com.linh.ecommerce.product;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.linh.ecommerce.category.Category;
+import com.linh.ecommerce.inventory.Inventory;
+import com.linh.ecommerce.size.Size;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +12,7 @@ import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -30,4 +31,20 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "product_categories",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<Category> categories;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "product_sizes",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "size_id")
+    )
+    private List<Size> sizes;
 }
